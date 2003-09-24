f7cc2841545f8d2dde1365b709163c371e26f915
Robert Leland <rleland@apache.org>
2003-09-24 03:44:10 +0000

    Bug#:	18993, and Bug#:	11520 Patches provided by  Greg Ludington,  Steve Stair, Saul Q Yuan Add ability of required to handle checkboxes, radio,select-one, and select-multiple field types.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@140093 13f79535-47bb-0310-9956-ffa450edef68

--- src/javascript/org/apache/commons/validator/javascript/validateRequired.js
+++ src/javascript/org/apache/commons/validator/javascript/validateRequired.js
@@ -34,11 +34,29 @@
                     fields[i++] = oRequired[x][1];
                     isValid = false;
                 }
-            } else if ((field.length > 0) && (field[0].type == 'radio')) {
+            } else if (field.type == "select-multiple") { 
+                var numOptions = field.options.length;
+                lastSelected=-1;
+                for(loop=numOptions-1;loop>=0;loop--) {
+                    if(field.options[loop].selected) {
+                        lastSelected = loop;
+                        value = field.options[loop].value;
+                        break;
+                    }
+                }
+                if(lastSelected < 0 || trim(value).length == 0) {
+                    if(i == 0) {
+                        focusField = field;
+                    }
+                    fields[i++] = oRequired[x][1];
+                    isValid=false;
+                }
+            } else if ((field.length > 0) && (field[0].type == 'radio' || field[0].type == 'checkbox')) {
                 isChecked=-1;
                 for (loop=0;loop < field.length;loop++) {
                     if (field[loop].checked) {
                         isChecked=loop;
+                        break; // only one needs to be checked
                     }
                 }
                 if (isChecked < 0) {
