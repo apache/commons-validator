9011574dd3e97d1574e02aa159b61594a45cca9a
Robert Leland <rleland@apache.org>
2003-12-15 08:09:56 +0000

    Change so CVS directory not copied.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@140119 13f79535-47bb-0310-9956-ffa450edef68

--- etc/jsdoc.sh
+++ etc/jsdoc.sh
@@ -5,4 +5,5 @@ outputto=$3
 package=/org/apache/commons/validator/javascript
 perl ${cmdpath}/jsdoc.pl --project-summary ${dirtoprocess}${package}/package.html --project-name "Package org.apache.commons.validator.javascript" --page-footer "Copyright © 2000-2003 - Apache Software Foundation
 " -d ${outputto}/${package} -r $dirtoprocess
-cp -R ${dirtoprocess}/${package}/doc-files $outputto/${package}
+mkdir $outputto/${package}/doc-files
+cp ${dirtoprocess}/${package}/doc-files/*.* $outputto/${package}/doc-files
