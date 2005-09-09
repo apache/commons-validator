da18885cbedbd828e5cc3bd5dee11b8ae314ac36
Donald J. Brown <mrdon@apache.org>
2005-09-09 23:08:35 +0000

    * Fixed jsdoc to run, assuming it is installed.  Still not integrated    with maven so requires 'ant javadoc.js'  * Changed compile to target 1.3
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@279907 13f79535-47bb-0310-9956-ffa450edef68

--- etc/jsdoc.sh
+++ etc/jsdoc.sh
@@ -27,7 +27,7 @@
 dirtoprocess=$2
 outputto=$3
 package=/org/apache/commons/validator/javascript
-perl ${cmdpath}/jsdoc.pl --project-summary ${dirtoprocess}${package}/package.html --project-name "Package org.apache.commons.validator.javascript" --page-footer "Copyright © 2000-2003 - Apache Software Foundation
+perl $1/jsdoc.pl --project-summary ${dirtoprocess}${package}/package.html --project-name "Package org.apache.commons.validator.javascript" --page-footer "Copyright © 2000-2003 - Apache Software Foundation
 " -d ${outputto}/${package} -r $dirtoprocess
-mkdir $outputto/${package}/doc-files
+mkdir -p $outputto/${package}/doc-files
 cp ${dirtoprocess}/${package}/doc-files/*.* $outputto/${package}/doc-files
