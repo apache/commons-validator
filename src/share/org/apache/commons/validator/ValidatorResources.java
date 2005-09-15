c54b207116a89948818173c27de3b6c6f325cc0f
Niall Kegan Pemberton <niallp@apache.org>
2005-09-15 00:40:52 +0000

    Add digester rule so that arg0-arg3 elements are not ignored when using DTD version prior to 1.2.0. This change is in a private method and makes Validator 1.2.0 backward compatible with version 1.1.3 of the DTD.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@287172 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/commons/validator/ValidatorResources.java
+++ src/share/org/apache/commons/validator/ValidatorResources.java
@@ -32,10 +32,12 @@ import java.util.Map;
 
 import org.apache.commons.collections.FastHashMap;
 import org.apache.commons.digester.Digester;
+import org.apache.commons.digester.Rule;
 import org.apache.commons.digester.xmlrules.DigesterLoader;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
 
 /**
  * <p>
@@ -200,6 +202,9 @@ public class ValidatorResources implements Serializable {
         digester.setValidating(true);
         digester.setUseContextClassLoader(true);
 
+        // Add rules for arg0-arg3 elements
+        addOldArgRules(digester);
+
         // register DTDs
         for (int i = 0; i < registrations.length; i += 2) {
             URL url = this.getClass().getResource(registrations[i + 1]);
@@ -208,6 +213,45 @@ public class ValidatorResources implements Serializable {
             }
         }
         return digester;
+    }
+
+    private static final String argsPattern 
+               = "form-validation/formset/form/field/arg";
+
+    /**
+     * Create a <code>Rule</code> to handle <code>arg0-arg3</code>
+     * elements. This will allow validation.xml files that use the
+     * versions of the DTD prior to Validator 1.2.0 to continue
+     * working.
+     */
+    private void addOldArgRules(Digester digester) {
+
+        // Create a new rule to process args elements
+        Rule rule = new Rule() {
+            public void begin(String namespace, String name, 
+                               Attributes attributes) throws Exception {
+                // Create the Arg
+                Arg arg = new Arg();
+                arg.setKey(attributes.getValue("key"));
+                arg.setName(attributes.getValue("name"));
+                try {
+                    arg.setPosition(Integer.parseInt(name.substring(3)));
+                } catch (Exception ex) {
+                    log.error("Error parsing Arg position: " 
+                               + name + " " + arg + " " + ex);
+                }
+
+                // Add the arg to the parent field
+                ((Field)digester.peek(0)).addArg(arg);
+            }
+        };
+
+        // Add the rule for each of the arg elements
+        digester.addRule(argsPattern + "0", rule);
+        digester.addRule(argsPattern + "1", rule);
+        digester.addRule(argsPattern + "2", rule);
+        digester.addRule(argsPattern + "3", rule);
+
     }
 
     /**
