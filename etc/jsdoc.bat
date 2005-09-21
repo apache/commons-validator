2999c2239a3cec315abd7c0ede50b5251ca73332
Niall Kegan Pemberton <niallp@apache.org>
2005-09-21 20:20:14 +0000

    Add windows JSDoc executable to maven & ant build.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@290820 13f79535-47bb-0310-9956-ffa450edef68

---
+++ etc/jsdoc.bat
@@ -0,0 +1,39 @@
+@echo off
+
+rem * $Id$
+rem * ====================================================================
+rem * Copyright  2004 The Apache Software Foundation
+rem *
+rem * Licensed under the Apache License, Version 2.0 (the "License");
+rem * you may not use this file except in compliance with the License.
+rem * You may obtain a copy of the License at
+rem *
+rem *     http://www.apache.org/licenses/LICENSE-2.0
+rem *
+rem * Unless required by applicable law or agreed to in writing, software
+rem * distributed under the License is distributed on an "AS IS" BASIS,
+rem * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem * See the License for the specific language governing permissions and
+rem * limitations under the License.
+
+rem *--------------------------------------------------------------*
+rem *                                                              *
+rem * JSDoc is a perl script for generating javadoc for javascript.*
+rem *                                                              *
+rem * The latest version can be downloaded from :                  *
+rem *                                                              *
+rem *         http://sourceforge.net/projects/jsdoc/               *
+rem *                                                              *
+rem * N.B. You also need something like ActivePerl installed to    *
+rem *      run this on windows. See the Installation instructions: *
+rem *                                                              *
+rem *         http://jsdoc.sourceforge.net/#install                *
+rem *                                                              *
+rem *--------------------------------------------------------------*
+
+set _PACKAGE=/org/apache/commons/validator/javascript
+set _JSDOC_HOME=%1
+set _JAVASCRIPT_DIR=%2%_PACKAGE%
+set _OUTPUT_DIR=%3
+set _COPYRIGHT="Copyright © 2000-2005 - Apache Software Foundation"
+perl %_JSDOC_HOME%/jsdoc.pl --project-summary %_JAVASCRIPT_DIR%/package.html --project-name "Package %_PACKAGE%" --page-footer %_COPYRIGHT% -d %_OUTPUT_DIR% %_JAVASCRIPT_DIR% 
--- etc/jsdoc.sh
+++ etc/jsdoc.sh
@@ -27,7 +27,5 @@
 dirtoprocess=$2
 outputto=$3
 package=/org/apache/commons/validator/javascript
-perl $1/jsdoc.pl --project-summary ${dirtoprocess}${package}/package.html --project-name "Package org.apache.commons.validator.javascript" --page-footer "Copyright © 2000-2003 - Apache Software Foundation
+perl $1/jsdoc.pl --project-summary ${dirtoprocess}${package}/package.html --project-name "Package org.apache.commons.validator.javascript" --page-footer "Copyright © 2000-2005 - Apache Software Foundation
 " -d ${outputto}/${package} -r $dirtoprocess
-mkdir -p $outputto/${package}/doc-files
-cp ${dirtoprocess}/${package}/doc-files/*.* $outputto/${package}/doc-files
