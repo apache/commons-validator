c772b42fd6a49bc009c818b04c473596fe58a97c
Henri Yandell <bayard@apache.org>
2006-10-06 21:44:55 +0000

    Adding _ and ; to the list of allowed characters within the path component of a URL. _ is definitely allowed and ; is allowed but there are restrictions on its use and we'll need to refactor UrlValidator to properly understand how a ; may be used for path parameterization. This is with reference to #VALIDATOR-204.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/validator/trunk@453784 13f79535-47bb-0310-9956-ffa450edef68

--- src/share/org/apache/commons/validator/UrlValidator.java
+++ src/share/org/apache/commons/validator/UrlValidator.java
@@ -148,7 +148,7 @@ public class UrlValidator implements Serializable {
      */
     private static final int PARSE_AUTHORITY_EXTRA = 3;
 
-    private static final String PATH_PATTERN = "/^(/[-\\w:@&?=+,.!/~*'%$]*)?$/";
+    private static final String PATH_PATTERN = "/^(/[-\\w:@&?=+,.!/~*'%$_;]*)?$/";
 
     private static final String QUERY_PATTERN = "/^(.*)$/";
 
