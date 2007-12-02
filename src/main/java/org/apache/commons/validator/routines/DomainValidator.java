dcd8c5aa35a4418adeecd6543056255e8ca8886c
Benjamin Speakmon <bspeakmon@apache.org>
2007-12-02 03:39:09 +0000

    VALIDATOR-252: improve regex performance for DomainValidator
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@600231 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/validator/routines/DomainValidator.java
+++ src/main/java/org/apache/commons/validator/routines/DomainValidator.java
@@ -63,7 +63,7 @@ import java.util.List;
 public class DomainValidator implements Serializable {
 
     // Regular expression strings for hostnames (derived from RFC2396 and RFC 1123)
-    private static final String DOMAIN_LABEL_REGEX = "\\p{Alnum}(?:[\\p{Alnum}-]*\\p{Alnum})*";
+    private static final String DOMAIN_LABEL_REGEX = "\\p{Alnum}(?>[\\p{Alnum}-]*\\p{Alnum})*";
     private static final String TOP_LABEL_REGEX = "\\p{Alpha}{2,}";
     private static final String DOMAIN_NAME_REGEX =
             "^(?:" + DOMAIN_LABEL_REGEX + "\\.)+" + "(" + TOP_LABEL_REGEX + ")$";
