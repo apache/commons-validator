06ebf916dd7d88f9e5fbdf291f870dd662c4be9e
Sebastian Bazley <sebb@apache.org>
2013-02-01 11:48:59 +0000

    Javadoc tweak
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@1441399 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigit.java
@@ -21,7 +21,7 @@ import java.io.Serializable;
 /**
  * <b>IBAN</b> (International Bank Account Number) Check Digit calculation/validation.
  * <p>
- * This rountine is based on the ISO 7064 Mod 97,10 check digit caluclation routine.
+ * This routine is based on the ISO 7064 Mod 97,10 check digit calculation routine.
  * <p>
  * The two check digit characters in a IBAN number are the third and fourth characters
  * in the code. For <i>check digit</i> calculation/validation the first four characters are moved
@@ -30,6 +30,8 @@ import java.io.Serializable;
  *  <code>CC</code> is the country code and <code>DD</code> is the check digit). For
  *  check digit calcualtion the check digit value should be set to zero (i.e.
  *  <code>CC00nnnnnnn</code> in this example.
+ * <p>
+ * Note: the class does not check the format of the IBAN number, only the check digits.
  * <p>
  * For further information see
  *  <a href="http://en.wikipedia.org/wiki/International_Bank_Account_Number">Wikipedia -
@@ -56,7 +58,7 @@ public final class IBANCheckDigit implements CheckDigit, Serializable {
     }
 
     /**
-     * Validate the check digit for an the IBAN code.
+     * Validate the check digit of an IBAN code.
      *
      * @param code The code to validate
      * @return <code>true</code> if the check digit is valid, otherwise
@@ -81,7 +83,7 @@ public final class IBANCheckDigit implements CheckDigit, Serializable {
      * characters and and should contain value "<code>00</code>".
      *
      * @param code The code to calculate the Check Digit for
-     * @return The calculated Check Digit
+     * @return The calculated Check Digit as 2 numeric decimal characters, e.g. "42"
      * @throws CheckDigitException if an error occurs calculating
      * the check digit for the specified code
      */
