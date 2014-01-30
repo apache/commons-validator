09482702c45653471a077734e560fe2d8fb9c7c4
Sebastian Bazley <sebb@apache.org>
2014-01-30 12:41:25 +0000

    Unused import
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@1562801 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigitTest.java
+++ src/test/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigitTest.java
@@ -17,7 +17,6 @@
 package org.apache.commons.validator.routines.checkdigit;
 
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
