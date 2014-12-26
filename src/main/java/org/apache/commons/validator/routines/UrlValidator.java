f5fb16346951d90fa263875d448824e150d97fb7
Benedikt Ritter <britter@apache.org>
2014-12-26 12:44:11 +0000

    PMD: Combine nested if statements
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@1647967 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/validator/routines/UrlValidator.java
+++ src/main/java/org/apache/commons/validator/routines/UrlValidator.java
@@ -339,11 +339,8 @@ public class UrlValidator implements Serializable {
             return false;
         }
 
-        if (isOff(ALLOW_ALL_SCHEMES)) {
-
-            if (!allowedSchemes.contains(scheme)) {
-                return false;
-            }
+        if (isOff(ALLOW_ALL_SCHEMES) && !allowedSchemes.contains(scheme)) {
+            return false;
         }
 
         return true;
@@ -361,10 +358,8 @@ public class UrlValidator implements Serializable {
         }
 
         // check manual authority validation if specified
-        if (authorityValidator != null) {
-            if (authorityValidator.isValid(authority)) {
-                return true;
-            }
+        if (authorityValidator != null && authorityValidator.isValid(authority)) {
+            return true;
         }
 
         Matcher authorityMatcher = AUTHORITY_PATTERN.matcher(authority);
@@ -387,10 +382,8 @@ public class UrlValidator implements Serializable {
         }
 
         String port = authorityMatcher.group(PARSE_AUTHORITY_PORT);
-        if (port != null) {
-            if (!PORT_PATTERN.matcher(port).matches()) {
-                return false;
-            }
+        if (port != null && !PORT_PATTERN.matcher(port).matches()) {
+            return false;
         }
 
         String extra = authorityMatcher.group(PARSE_AUTHORITY_EXTRA);
@@ -422,10 +415,8 @@ public class UrlValidator implements Serializable {
 
         int slashCount = countToken("/", path);
         int dot2Count = countToken("..", path);
-        if (dot2Count > 0) {
-            if ((slashCount - slash2Count - 1) <= dot2Count) {
-                return false;
-            }
+        if (dot2Count > 0 && (slashCount - slash2Count - 1) <= dot2Count) {
+            return false;
         }
 
         return true;
