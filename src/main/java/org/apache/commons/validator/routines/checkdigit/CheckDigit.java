718a4e20883e59249faeedec17a25bf507ba1bfe
Sebastian Bazley <sebb@apache.org>
2015-01-04 03:22:50 +0000

    Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@1649287 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/validator/routines/checkdigit/CheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/CheckDigit.java
@@ -53,6 +53,7 @@ public interface CheckDigit {
      * Calculates the <i>Check Digit</i> for a code.
      *
      * @param code The code to calculate the Check Digit for.
+     * The string must not include the check digit
      * @return The calculated Check Digit
      * @throws CheckDigitException if an error occurs.
      */
@@ -61,7 +62,7 @@ public interface CheckDigit {
     /**
      * Validates the check digit for the code.
      *
-     * @param code The code to validate.
+     * @param code The code to validate, the string must include the check digit.
      * @return <code>true</code> if the check digit is valid, otherwise
      * <code>false</code>.
      */
