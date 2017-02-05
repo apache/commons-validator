0d886c2358c2417a402b0d0fd575d8714c98d043
Sebastian Bazley <sebb@apache.org>
2017-02-05 18:46:45 +0000

    VALIDATOR-379 CodeValidator unconditionally trim()s the input string
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/validator/trunk@1781789 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/validator/routines/CodeValidator.java
+++ src/main/java/org/apache/commons/validator/routines/CodeValidator.java
@@ -218,6 +218,13 @@ public final class CodeValidator implements Serializable {
     /**
      * Validate the code returning either <code>true</code>
      * or <code>false</code>.
+     * <p>
+     * This calls {@link #validate(String)} and returns false
+     * if the return value is null, true otherwise.
+     * <p>
+     * Note that {@link #validate(String)} trims the input
+     * and if there is a {@link RegexValidator} it may also
+     * change the input as part of the validation.
      *
      * @param input The code to validate
      * @return <code>true</code> if valid, otherwise
@@ -230,6 +237,10 @@ public final class CodeValidator implements Serializable {
     /**
      * Validate the code returning either the valid code or
      * <code>null</code> if invalid.
+     * <p>
+     * Note that this method trims the input
+     * and if there is a {@link RegexValidator} it may also
+     * change the input as part of the validation.
      *
      * @param input The code to validate
      * @return The code if valid, otherwise <code>null</code>
