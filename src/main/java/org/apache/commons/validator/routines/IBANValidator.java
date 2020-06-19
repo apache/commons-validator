b5aa1894a9ad51bb1af57031ead7ed1ffe88e4a3
Sebb <sebb@apache.org>
2020-06-19 13:29:47 +0100

    Unused import

--- src/main/java/org/apache/commons/validator/routines/IBANValidator.java
+++ src/main/java/org/apache/commons/validator/routines/IBANValidator.java
@@ -20,7 +20,6 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.validator.routines.RegexValidator;
 import org.apache.commons.validator.routines.checkdigit.IBANCheckDigit;
 
 /**
