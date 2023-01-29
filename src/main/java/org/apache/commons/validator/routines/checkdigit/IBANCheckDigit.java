a660f0dd9a58c310af2052c9914fb4321a2cc954
Gary Gregory <garydgregory@gmail.com>
2023-01-29 14:57:03 -0500

    Format tweaks

--- src/main/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/IBANCheckDigit.java
@@ -72,14 +72,14 @@ public final class IBANCheckDigit implements CheckDigit, Serializable {
         if (code == null || code.length() < MIN_CODE_LEN) {
             return false;
         }
-        final String check = code.substring(2,4); // CHECKSTYLE IGNORE MagicNumber
+        final String check = code.substring(2, 4); // CHECKSTYLE IGNORE MagicNumber
         if ("00".equals(check) || "01".equals(check) || "99".equals(check)) {
             return false;
         }
         try {
             final int modulusResult = calculateModulus(code);
             return modulusResult == 1;
-        } catch (final CheckDigitException  ex) {
+        } catch (final CheckDigitException ex) {
             return false;
         }
     }
@@ -98,8 +98,7 @@ public final class IBANCheckDigit implements CheckDigit, Serializable {
     @Override
     public String calculate(String code) throws CheckDigitException {
         if (code == null || code.length() < MIN_CODE_LEN) {
-            throw new CheckDigitException("Invalid Code length=" +
-                    (code == null ? 0 : code.length()));
+            throw new CheckDigitException("Invalid Code length=" + (code == null ? 0 : code.length()));
         }
         code = code.substring(0, 2) + "00" + code.substring(4); // CHECKSTYLE IGNORE MagicNumber
         final int modulusResult = calculateModulus(code);
@@ -122,8 +121,7 @@ public final class IBANCheckDigit implements CheckDigit, Serializable {
         for (int i = 0; i < reformattedCode.length(); i++) {
             final int charValue = Character.getNumericValue(reformattedCode.charAt(i));
             if (charValue < 0 || charValue > MAX_ALPHANUMERIC_VALUE) {
-                throw new CheckDigitException("Invalid Character[" +
-                        i + "] = '" + charValue + "'");
+                throw new CheckDigitException("Invalid Character[" + i + "] = '" + charValue + "'");
             }
             total = (charValue > 9 ? total * 100 : total * 10) + charValue; // CHECKSTYLE IGNORE MagicNumber
             if (total > MAX) {
