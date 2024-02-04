2118daa18ab94568162ebfe718c8863a23908cf0
Gary Gregory <garydgregory@gmail.com>
2024-02-04 14:08:59 -0500

    Explicit call to super is redundant

--- src/main/java/org/apache/commons/validator/routines/checkdigit/LuhnCheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/LuhnCheckDigit.java
@@ -50,7 +50,6 @@ public final class LuhnCheckDigit extends ModulusCheckDigit {
      * Constructs a modulus 10 Luhn Check Digit routine.
      */
     public LuhnCheckDigit() {
-        super();
     }
 
     /**
