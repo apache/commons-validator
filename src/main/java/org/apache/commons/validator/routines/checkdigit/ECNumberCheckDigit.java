1f6eec60a29c14d510be85f4796cbec859609637
homebeaver <homebeaver@outlook.de>
2024-05-04 17:01:24 +0200

    build (17) (whitespace) FileTabCharacter: File contains tab characters

--- src/main/java/org/apache/commons/validator/routines/checkdigit/ECNumberCheckDigit.java
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/ECNumberCheckDigit.java
@@ -102,14 +102,14 @@ public final class ECNumberCheckDigit extends ModulusCheckDigit {
         Object cde = REGEX_VALIDATOR.validate(code);
         if (cde instanceof String) {
             try {
-        	    final int modulusResult = calculateModulus((String)cde, true);
-//        		System.out.println("modulusResult="+modulusResult + " for code "+code);
-        		return modulusResult == Character.getNumericValue(code.charAt(code.length() - 1));
-        	} catch (final CheckDigitException ex) {
-        		return false;
-        	}
+                final int modulusResult = calculateModulus((String)cde, true);
+//              System.out.println("modulusResult="+modulusResult + " for code "+code);
+                return modulusResult == Character.getNumericValue(code.charAt(code.length() - 1));
+            } catch (final CheckDigitException ex) {
+                return false;
+            }
         } else {
-        	return false;
+            return false;
         }
     }
 
