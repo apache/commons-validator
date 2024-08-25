63dadc4e9f4f7bfb3fb3e7e4a9619297bb7f257c
Gary Gregory <garydgregory@gmail.com>
2024-08-25 10:46:40 -0400

    Sort imports

--- src/test/java/org/apache/commons/validator/routines/ISSNValidatorTest.java
+++ src/test/java/org/apache/commons/validator/routines/ISSNValidatorTest.java
@@ -17,10 +17,10 @@
 package org.apache.commons.validator.routines;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
