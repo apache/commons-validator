6636af423096ec9d308aabfca7b5a22532f928c8
homebeaver <homebeaver@outlook.de>
2024-10-02 18:30:12 +0200

    add Swedish VATIN check digit validator and test
    https://issues.apache.org/jira/projects/VALIDATOR/issues/VALIDATOR-494

---
+++ src/main/java/org/apache/commons/validator/routines/checkdigit/VATidSECheckDigit.java
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.validator.routines.checkdigit;
+
+import org.apache.commons.validator.GenericValidator;
+
+/**
+ * Swedish VAT identification number (VATIN) Check Digit calculation/validation.
+ * <p>
+ * Registreringsnummer för mervärdesskatt (Momsnummer MomsNr.)
+ * </p>
+ * <p>
+ * See <a href="https://en.wikipedia.org/wiki/VAT_identification_number">Wikipedia</a>
+ * for more details.
+ * </p>
+ *
+ * @since 1.10.0
+ */
+// LuhnCheckDigit ist final, cannot be subclassed
+public final class VATidSECheckDigit extends ModulusCheckDigit {
+
+    private static final long serialVersionUID = 4544090452894549769L;
+
+    /** Singleton Check Digit instance */
+    private static final VATidSECheckDigit INSTANCE = new VATidSECheckDigit();
+
+    /**
+     * Gets the singleton instance of this validator.
+     * @return A singleton instance of the class.
+     */
+    public static CheckDigit getInstance() {
+        return INSTANCE;
+    }
+
+    static final int LEN = 10; // without suffix "01"
+
+    /** Weighting given to digits depending on their right position */
+    private static final int[] POSITION_WEIGHT = {2, 1};
+
+    /**
+     * Constructs a Check Digit routine.
+     */
+    private VATidSECheckDigit() {
+        super(MODULUS_10);
+    }
+
+    /**
+     * <p>Calculates the <i>weighted</i> value of a character in the
+     * code at a specified position.</p>
+     *
+     * <p>For Luhn (from right to left) <b>odd</b> digits are weighted
+     * with a factor of <b>one</b> and <b>even</b> digits with a factor
+     * of <b>two</b>. Weighted values &gt; 9, have 9 subtracted</p>
+     *
+     * @param charValue The numeric value of the character.
+     * @param leftPos The position of the character in the code, counting from left to right
+     * @param rightPos The position of the character in the code, counting from right to left
+     * @return The weighted value of the character.
+     */
+    @Override
+    protected int weightedValue(final int charValue, final int leftPos, final int rightPos) {
+        final int weight = POSITION_WEIGHT[rightPos % 2]; // CHECKSTYLE IGNORE MagicNumber
+        final int weightedValue = charValue * weight;
+        return weightedValue > 9 ? weightedValue - 9 : weightedValue; // CHECKSTYLE IGNORE MagicNumber
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isValid(final String ocode) {
+        String code = ocode;
+        if (GenericValidator.isBlankOrNull(code)) {
+            return false;
+        }
+        if (code.length() > LEN) {
+            if (code.length() != LEN + 2) {
+                return false;
+            }
+            if (!code.endsWith("01")) {
+                return false;
+            }
+            code = code.substring(0, LEN);
+        }
+
+        try {
+            final int modulusResult = calculateModulus(code, true);
+            return modulusResult == 0;
+        } catch (final CheckDigitException ex) {
+            return false;
+        }
+    }
+
+}
---
+++ src/test/java/org/apache/commons/validator/routines/checkdigit/VATidSECheckDigitTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.validator.routines.checkdigit;
+
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * SE VAT Id Check Digit Tests.
+ */
+public class VATidSECheckDigitTest extends AbstractCheckDigitTest {
+
+   /**
+     * Sets up routine & valid codes.
+
+     without last 2 chars
+
+    SE 1366959755 01 : valide, aber ungültig. Quelle bmf.gv.at
+    SE 5561888404 01 : gültig OLLE SVENSSONS PARTIAFFÄR AKTIEBOLAG. Quelle pruefziffernberechnung.de
+    SE 1234567897 01 : valide, aber ungültig. Quelle https://old.formvalidation.io/validators/vat/
+    SE 5560528514 01 : gültig Scandinavian Eyewear AB, JÖNKÖPING. Quelle https://www.adresslabor.de/en/products/vat-id-no-check.html
+    SE 5566801444 01 : gültig XLN Audio AB, STOCKHOLM
+    SE 5565102471 01 : gültig amo kraftkabel AB, ALSTERMO
+     */
+    @BeforeEach
+    protected void setUp() {
+        routine = VATidSECheckDigit.getInstance();
+        valid = new String[] {"0000000018" // theoretical minimum (invalid, because starts with 0)
+            , "1366959755", "5561888404", "1234567897", "5560528514", "5566801444", "5565102471"
+            , "5565102570" // checkdigit zero
+            , "9999999999" // theoretical maximum
+            };
+    }
+
+}
