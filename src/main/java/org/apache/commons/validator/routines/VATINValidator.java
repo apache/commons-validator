9393a5b813f0a43b85ea3e5f7d735331de0ad006
homebeaver <homebeaver@outlook.de>
2024-10-03 14:28:14 +0200

    VAT identification number (VATIN) Validator / main routine (first shot)

---
+++ src/main/java/org/apache/commons/validator/routines/VATINValidator.java
@@ -0,0 +1,288 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.validator.routines;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.apache.commons.validator.routines.checkdigit.VATINCheckDigit;
+
+// kopiert aus IBANValidator TODO kommentieren + implementieren
+/**
+ * VAT identification number (VATIN) Validator.
+ * <p>
+ * The validator includes a default set of formats for European countries, see
+ * https://en.wikipedia.org/wiki/VAT_identification_number
+ * </p>
+// * <p>
+// * This can get out of date, but the set can be adjusted by creating a validator and using the
+// * {@link #setValidator(String, int, String)} or
+// * {@link #setValidator(Validator)}
+// * method to add (or remove) an entry.
+// * </p>
+// * <p>
+// * For example:
+// * </p>
+// * <pre>
+// * IBANValidator ibv = new IBANValidator();
+// * ibv.setValidator("XX", 12, "XX\\d{10}")
+// * </pre>
+// * <p>
+// * The singleton default instance cannot be modified in this way.
+ * </p>
+ * @since 1.9.0
+ */
+public class VATINValidator {
+
+    /**
+     * The validation class
+     */
+    public static class Validator {
+        /*
+         * The minimum length does not appear to be defined.
+         * Denmark, Finnland are currently the shortest (including countryCode).
+         */
+        private static final int MIN_LEN = 10;
+        private static final int MAX_LEN = 16; // non EU: China 18
+
+        final String countryCode;
+        final String[] otherCountryCodes;
+        final RegexValidator regexValidator;
+        final int vatinLength; // used to avoid unnecessary regex matching
+
+        /**
+         * Creates the validator.
+         * @param countryCode the country code
+         * @param maxLength the max length of the VATIN including country code
+         * @param regexWithCC the regex to use to check the format, the regex MUST start with the country code.
+         */
+        public Validator(final String countryCode, final int maxLength, final String regexWithCC) {
+            this(countryCode, maxLength, regexWithCC.substring(countryCode.length()), new String[] {});
+        }
+
+        /**
+         * Creates the validator.
+         * @param countryCode the country code
+         * @param maxLength the max length of the VATIN including country code
+         * @param regexWithoutCC the regex to use to check the format, the regex MUST NOT start with the country code.
+         */
+        Validator(final String countryCode, final int maxLength, final String regexWithoutCC, final String... otherCountryCodes) {
+            if (!(countryCode.length() == 2 && Character.isUpperCase(countryCode.charAt(0)) && Character.isUpperCase(countryCode.charAt(1)))) {
+                throw new IllegalArgumentException("Invalid country Code; must be exactly 2 upper-case characters");
+            }
+            if (maxLength > MAX_LEN || maxLength < MIN_LEN) {
+                throw new IllegalArgumentException("Invalid length parameter, must be in range " + MIN_LEN + " to " + MAX_LEN + " inclusive: " + maxLength);
+            }
+            final String regex = countryCode + regexWithoutCC;
+            if (!regex.startsWith(countryCode)) {
+                throw new IllegalArgumentException("countryCode '" + countryCode + "' does not agree with format: " + regex);
+            }
+            this.countryCode = countryCode;
+            this.otherCountryCodes = otherCountryCodes.clone();
+            final List<String> regexList = new ArrayList<>(this.otherCountryCodes.length + 1);
+            regexList.add(countryCode + regexWithoutCC);
+            for (final String otherCc : otherCountryCodes) {
+                regexList.add(otherCc + regexWithoutCC);
+            }
+            this.vatinLength = maxLength;
+            this.regexValidator = new RegexValidator(regexList);
+        }
+
+        /**
+         * Gets the RegexValidator.
+         *
+         * @return the RegexValidator.
+         * @since 1.8
+         */
+        public RegexValidator getRegexValidator() {
+            return regexValidator;
+        }
+    }
+
+    private static final int SHORT_CODE_LEN = 2;
+
+/*
+
+Foreign companies that trade with private individuals and non-business organisations in the EU may have a VATIN
+starting with "EU" instead of a country code, e.g. Godaddy EU826010755
+                                          and Amazon (AWS) EU826009064.
+ */
+    private static final Validator[] DEFAULT_VALIDATORS = {                   //
+            new Validator("AT", 11, "ATU\\d{8}"),                             // Austria  ATU9999999p
+            new Validator("BE", 12, "BE[0-1]\\d{9}"),                         // Belgium  BE99999999pp
+            // altes Format 9-stellig ist ungültig !!!
+            new Validator("BG", 12, "BG(\\d)?\\d{9}"),                        // Bulgaria BG99999999(9)p
+            new Validator("CY", 11, "CY[013459]\\d{7}[A-Z]"),                 // Cyprus   CY99999999L
+            new Validator("CZ", 12, "CZ(\\d)?(\\d)?\\d{8}"),                  // Czechia  CZ9999999(99)p
+            new Validator("DE", 11, "DE\\d{9}"),                              // Germany  DE99999999p
+            new Validator("DK", 10, "DK[1-9]\\d{7}"),                         // Denmark  DK99999999
+            new Validator("EE", 11, "EE\\d{9}"),                              // Estonia  EE99999999p
+            new Validator("EL", 11, "EL\\d{9}"),                              // Greece   EL99999999p
+            new Validator("ES", 11, "ES[A-Z0-9]\\d{7}[A-Z0-9]"),              // Spain    ESX9999999P
+            new Validator("EU", 11, "EU\\d{9}"),                              // Foreign companies
+            new Validator("FI", 10, "FI\\d{8}"),                              // Finland  FI9999999p
+            new Validator("FR", 13, "FR[A-Z0-9]{2}\\d{9}"),                   // France   FRXX999999999
+            new Validator("HR", 13, "HR\\d{11}"),                             // Croatia  HR9999999999p
+            new Validator("HU", 10, "HU\\d{8}"),                              // Hungary  HU9999999p
+            new Validator("IE", 11, "IE\\d{7}[A-W]([A-I])?"),                 // Ireland  IE9999999a(A)
+            new Validator("IT", 13, "IT\\d{11}"),                             // Italy    IT9999999999p
+            // optional Group for Temporarily Registered Taxpayers with 12 digits, C11==1
+            new Validator("LT", 14, "LT\\d{9}([0-9]1[0-9])?"),                // Lithuania LT99999999p or 12 digits
+            new Validator("LU", 13, "LU\\d{8}"),                              // Luxembourg LU999999pp
+            new Validator("LV", 13, "LV[4-9]\\d{10}"),                        // Latvia      LV9999999999p
+            new Validator("MT", 14, "MT\\d{8}"),                              // Malta       MT999999pp
+            new Validator("NL", 14, "NL\\d{9}B\\d{2}"),                       // Netherlands NL99999999pB01
+            new Validator("PL", 12, "PL\\d{10}"),                             // Poland      PL999999999p
+            new Validator("PT", 11, "PT\\d{9}"),                              // Portugal    PT99999999p
+            new Validator("RO", 12, "RO[1-9](\\d)?(\\d)?(\\d)?(\\d)?(\\d)?(\\d)?(\\d)?(\\d)?\\d"), // Romania     RO999999999p
+            new Validator("SE", 14, "SE\\d{10}01"),                           // Sweden      SE999999999p01
+            new Validator("SI", 10, "SI[1-9]\\d{7}"),                         // Slovenia    SI19999999p
+            // ne of 2, 3, 4, 7, 8, 9
+            new Validator("SK", 12, "SK[1-9]\\d[2-4,7-9]\\d{7}"),             // Slovakia    SK19999999999
+            new Validator("XI", 14, "XI(\\d{3})?\\d{9}"),                     // North.Ireland XI9999999999
+    };
+
+    /** The singleton instance which uses the default formats */
+    public static final VATINValidator DEFAULT_VATIN_VALIDATOR = new VATINValidator();
+
+    /**
+     * Gets the singleton instance of the IBAN validator using the default formats
+     *
+     * @return A singleton instance of the validator
+     */
+    public static VATINValidator getInstance() {
+        return DEFAULT_VATIN_VALIDATOR;
+    }
+
+    private final ConcurrentMap<String, Validator> validatorMap;
+
+    /**
+     * Create a default validator.
+     */
+    public VATINValidator() {
+        this(DEFAULT_VALIDATORS);
+    }
+
+    /**
+     * Create an IBAN validator from the specified map of IBAN formats.
+     *
+     * @param validators map of IBAN formats
+     */
+    public VATINValidator(final Validator[] validators) {
+        this.validatorMap = createValidators(validators);
+    }
+
+    private ConcurrentMap<String, Validator> createValidators(final Validator[] validators) {
+        final ConcurrentMap<String, Validator> map = new ConcurrentHashMap<>();
+        for (final Validator validator : validators) {
+            map.put(validator.countryCode, validator);
+            for (final String otherCC : validator.otherCountryCodes) {
+                map.put(otherCC, validator);
+            }
+        }
+        return map;
+    }
+
+    /**
+     * Gets a copy of the default Validators.
+     *
+     * @return a copy of the default Validator array
+     */
+    public Validator[] getDefaultValidators() {
+        return Arrays.copyOf(DEFAULT_VALIDATORS, DEFAULT_VALIDATORS.length);
+    }
+
+    /**
+     * Gets the Validator for a given IBAN
+     *
+     * @param code a string starting with the ISO country code (e.g. an IBAN)
+     *
+     * @return the validator or {@code null} if there is not one registered.
+     */
+    public Validator getValidator(final String code) {
+        if (code == null || code.length() < SHORT_CODE_LEN) { // ensure we can extract the key
+            return null;
+        }
+        final String key = code.substring(0, SHORT_CODE_LEN);
+        return validatorMap.get(key);
+    }
+
+    /**
+     * Does the class have the required validator?
+     *
+     * @param code the code to check
+     * @return true if there is a validator
+     */
+    public boolean hasValidator(final String code) {
+        return getValidator(code) != null;
+    }
+
+    /**
+     * Validate an VATIN Code
+     *
+     * @param code The value validation is being performed on
+     * @return {@code true} if the value is valid
+     */
+    public boolean isValid(final String code) {
+        final Validator formatValidator = getValidator(code);
+//        System.out.println("formatValidator.maxLength="+(formatValidator==null?-1:formatValidator.vatinLength));
+        if (formatValidator == null || code.length() > formatValidator.vatinLength || !formatValidator.regexValidator.isValid(code)) {
+            return false;
+        }
+        return VATINCheckDigit.getInstance().isValid(code);
+    }
+
+    /**
+     * Installs a validator.
+     * Will replace any existing entry which has the same countryCode.
+     *
+     * @param countryCode the country code
+     * @param length the length of the IBAN. Must be &ge; 8 and &le; 32.
+     * If the length is &lt; 0, the validator is removed, and the format is not used.
+     * @param format the format of the IBAN (as a regular expression)
+     * @return the previous Validator, or {@code null} if there was none
+     * @throws IllegalArgumentException if there is a problem
+     * @throws IllegalStateException if an attempt is made to modify the singleton validator
+     */
+    public Validator setValidator(final String countryCode, final int length, final String format) {
+        if (this == DEFAULT_VATIN_VALIDATOR) {
+            throw new IllegalStateException("The singleton validator cannot be modified");
+        }
+        if (length < 0) {
+            return validatorMap.remove(countryCode);
+        }
+        return setValidator(new Validator(countryCode, length, format));
+    }
+
+    /**
+     * Installs a validator.
+     * Will replace any existing entry which has the same countryCode
+     *
+     * @param validator the instance to install.
+     * @return the previous Validator, or {@code null} if there was none
+     * @throws IllegalStateException if an attempt is made to modify the singleton validator
+     */
+    public Validator setValidator(final Validator validator) {
+        if (this == DEFAULT_VATIN_VALIDATOR) {
+            throw new IllegalStateException("The singleton validator cannot be modified");
+        }
+        return validatorMap.put(validator.countryCode, validator);
+    }
+}
---
+++ src/test/java/org/apache/commons/validator/routines/VATINValidatorTest.java
@@ -0,0 +1,229 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.validator.routines;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.apache.commons.validator.routines.checkdigit.VATINCheckDigit;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests {@link VATINValidator}.
+ */
+public class VATINValidatorTest {
+
+    private static final VATINValidator VALIDATOR = VATINValidator.getInstance();
+
+    // Eclipse 3.6 allows you to turn off formatting by placing a special comment, like
+    // @formatter:off
+    private static final String[] VALID_VATIN_FIXTURES = {
+            "ATU10223006",  // aus BMF_UID_Konstruktionsregeln
+            "ATU13585627",  // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZAT
+            "ATU54065602",  // https://zukunftsregion-steyr.at/impressum
+            "BE0136695962", // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZBE (neues Format)
+            "BE0776091951", // aus BMF_UID_Konstruktionsregeln
+            "BG831650349",  // (Nestlé) НЕСТЛЕ БЪЛГАРИЯ - АД
+            "BG8001010008", // 8001010008 Мъж, с дата на раждане 01.01.1980 г.
+            "CY30010823A",  // LIDL Cyprus
+            "CZ29042828",   // SVĚT KÁVOVARŮ s.r.o., STRANČICE
+            "CZ6852294449", // from TIN_-_country_sheet_CZ_de.pdf
+            "DE000000011",  // theoretisches Minimum mit PZ
+            "DE136695976",  // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZDE
+            "DK88146328",   // aus BMF_UID_Konstruktionsregeln
+            "DK13585628",   // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZDK
+            "EE100931558",   // aus https://github.com/anghelvalentin/CountryValidator/blob/master/CountryValidator.Tests/CountriesValidators/EstoniaValidatorTests.cs
+            "EE100594102",   // MOVEK GRUPP OÜ - validiert mit https://apps.emta.ee/saqu/public/kmkrnr?lang=en
+            "EL998537832",   // TRYGONS ΑΕ
+            "EL094327684",   // GENERALI HELLAS Α Α Ε
+            "ESA60195278",   // LIDL SUPERMERCADOS, S.A.U.
+            "ES54362315K",   // Españoles con DNI
+            "ESB58378431",   // Sociedades de responsabilidad limitada
+            "ESX2482300W",   // Extranjeros residentes
+            "ESW8265365J",   // Establecimientos permanentes de entidades no residentes en España
+            "FI13669598",    // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZFI
+            "FI01745928",    // aus https://tarkistusmerkit.teppovuori.fi/tarkmerk.htm#y-tunnus2
+            "FI09853608",    // aus BMF_UID_Konstruktionsregeln
+            "FR40303265045", // SA SODIMAS; Adresse 11 RUE AMPERE, 26600 PONT DE L ISERE (validiert mit VIES)
+            "FRK7399859412", // SLRL ALTEA EXPERTISE COMPTABLE (validiert mit VIES)
+            "FR06399859412", "FRB0399859412", "FRY0399859412", "FRB1399859412", // not unique check digit
+            "FRT1399859412", "FRH2399859412", "FRN2399859412", "FRC3399859412", "FRU3399859412",
+            "FRZ3399859412", "FRJ4399859412", "FRP4399859412", "FRD5399859412", "FRJ5399859412",
+            "FRQ6399859412", "FRV6399859412", "FRE7399859412", "FRR8399859412", "FRW8399859412",
+            "FRL9399859412", "FRR9399859412", "FR0J399859412", "FR1J399859412", "FR2K399859412",
+            "FR3K399859412", "FR4K399859412", "FR5K399859412", "FR6K399859412", "FR6L399859412",
+            "FR7L399859412", "FR8L399859412", "FR9L399859412", "FR0W399859412", "FR1X399859412",
+            "FR2X399859412", "FR3X399859412", "FR4X399859412", "FR5X399859412", "FR6Y399859412",
+            "FR7Y399859412", "FR8Y399859412", "FR9Y399859412",
+            "FR37000005990", "FR64000059252", "FR03000116300", // special cases Monaco (not EU) with French VATIN (invalid SIREN)
+            "HR33392005961",  // NASTAVNI ZAVOD ZA JAVNO ZDRAVSTVO DR. ANDRIJA ŠTA, Zagreb
+            "HU12892312",     // CNCEDU KERESKEDELMI ÉS SZOLGÁLTATÓ KORLÁTOLT FELELŐSSÉGŰ TÁRSASÁG
+
+            "IE6388047V",     // GOOGLE IRELAND LIMITED
+            "IT00950501007",  // BANCA D'ITALIA (validiert mit VIES)
+            "IT02866820240",  // aus https://www.valbruna-stainless-steel.com/
+            "LT582708716",    // ASIMA
+            "LT100014579016", // Senoji rotonda
+            "LU25180625",     // snct.lu
+            "LV40003567907",  // Pasažieru vilciens
+            "MT20200019",     // BAJADA NEW ENERGY LIMITED, MRS3000 Marsa
+            "NL003660564B01", // STAM + DE KONING BOUW B.V. EINDHOVEN
+            "NL004495445B01", // OPENJONGERENVERENIGING DE KOORNBEURS, DELFT
+            "PL1060000062",   // aus https://pl.wikipedia.org/wiki/Numer_identyfikacji_podatkowej
+            "PT510728189",    // CLARANET II SOLUTIONS, S.A. PORTO
+            "RO27825131",     // NUTRISOYA SRL
+            "SE556680144401", // XLN Audio AB, STOCKHOLM
+            "SI21649405",     // STANOVANJSKO PODJETJE KONJICE D.O.O.
+            "SK2120567108",   // Markon s.r.o. Bratislava
+            "XI110305878",    // https://www.bullseyecountrysport.co.uk/contact-us-2-w.asp
+            "XI366303068",    // donnellygroup.co.uk
+    };
+    // @formatter:on
+
+    // @formatter:off
+    private static final String[] INVALID_VATIN_FIXTURES = {
+            "",                        // empty
+            "   ",                     // empty
+            "A",                       // too short
+            "AB",                      // too short
+            "AT99999999",     // too short, missing U:ATU
+            "ATu99999999",    // lowercase U:ATU
+            "ATU9999999",     // too short
+            "BE136695962",    // aus http://www.pruefziffernberechnung.de/U/USt-IdNr.shtml#PZBE (altes 9-stelliges Format)
+            "CY61234567I",    // Must not start with 2,6,7,8
+            "DE00000000",     // too short
+            "EL94327684",     // zu kurz, führende Null fehlt, korrekt: EL094327684
+            "GR023456780",    // falscher Prefix, sollte EL sein!
+            "ESA10215",       // too short
+            "LT100008668621", // C11 is not 1
+            "LV07091910933",  // aus SAP Format 2: Natural person
+            "LV07091910931",  // mit anderer Prüfziffer
+            "NL004495445B00", // Suffix "00"
+            "RO027825131",    // Must not start with 0
+            "SE136695975523", // Must not end with 23
+            "SI00000019",     // Must not start with 0
+            "SK0000000011",   // Must not start with 0
+            "SK1111111111",   // 3rd digit not be 1
+            "MC37000005990",  // No CheckDigit routine for Monaco
+    };
+    // @formatter:on
+
+    @Test
+    public void testGetRegexValidatortPatterns() {
+        assertNotNull(VALIDATOR.getValidator("DE").getRegexValidator().getPatterns(), "DE");
+    }
+
+    @Test
+    public void testGetValidator() {
+        assertNotNull(VALIDATOR.getValidator("FI"), "FI");
+        assertNull(VALIDATOR.getValidator("fi"), "fi");
+    }
+
+    @Test
+    public void testHasValidator() {
+        assertTrue(VALIDATOR.hasValidator("FI"), "FI");
+        assertFalse(VALIDATOR.hasValidator("fi"), "fi");
+    }
+
+    @Test
+    public void testInValid() {
+        for (final String f : INVALID_VATIN_FIXTURES) {
+            assertFalse(VALIDATOR.isValid(f), f);
+        }
+    }
+
+    @Test
+    public void testNull() {
+        assertFalse(VALIDATOR.isValid(null), "isValid(null)");
+    }
+
+    @Test
+    public void testSetDefaultValidator1() {
+        final IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> VALIDATOR.setValidator("GB", 15, "GB"));
+        assertThat(thrown.getMessage(), is(equalTo("The singleton validator cannot be modified")));
+    }
+
+    @Test
+    public void testSetDefaultValidator2() {
+        final IllegalStateException thrown = assertThrows(IllegalStateException.class, () -> VALIDATOR.setValidator("GB", -1, "GB"));
+        assertThat(thrown.getMessage(), is(equalTo("The singleton validator cannot be modified")));
+    }
+
+    @Test
+    public void testSetValidatorLC() {
+        final VATINValidator validator = new VATINValidator();
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> validator.setValidator("gb", 15, "GB"));
+        assertThat(thrown.getMessage(), is(equalTo("Invalid country Code; must be exactly 2 upper-case characters")));
+    }
+
+    @Test
+    public void testSetValidatorLen1() {
+        final VATINValidator validator = new VATINValidator();
+        assertNotNull(validator.setValidator("DE", -1, ""), "should be present");
+        assertNull(validator.setValidator("DE", -1, ""), "no longer present");
+    }
+
+    private static final String INVALID_LENGTH = "Invalid length parameter, must be in range 10 to 16 inclusive:";
+
+    @Test
+    public void testSetValidatorLen35() {
+        final VATINValidator validator = new VATINValidator();
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> validator.setValidator("DE", 35, "DE"));
+        System.out.println("thrown.getMessage():" + thrown.getMessage());
+        assertThat(thrown.getMessage(), is(equalTo(INVALID_LENGTH + " 35")));
+    }
+
+    @Test
+    public void testSetValidatorLen7() {
+        final VATINValidator validator = new VATINValidator();
+        final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> validator.setValidator("GB", 7, "GB"));
+        assertThat(thrown.getMessage(), is(equalTo(INVALID_LENGTH + " 7")));
+    }
+
+    @Test
+    public void testSorted() {
+        final VATINValidator validator = new VATINValidator();
+        final VATINValidator.Validator[] vals = validator.getDefaultValidators();
+        assertNotNull(vals);
+        for (int i = 1; i < vals.length; i++) {
+            if (vals[i].countryCode.compareTo(vals[i - 1].countryCode) <= 0) {
+                fail("Not sorted: " + vals[i].countryCode + " <= " + vals[i - 1].countryCode);
+            }
+        }
+    }
+
+    @Test
+    public void testValid() {
+        for (final String f : VALID_VATIN_FIXTURES) {
+// TODO 2 Zeilen raus  assertTrue(IBANCheckDigit.IBAN_CHECK_DIGIT.isValid(f), "Checksum fail: " + f);
+//            System.out.println("VAT:"+f);
+//            if("BE0136695962".equals(f)) {
+//                assertTrue(VATINCheckDigit.getInstance().isValid(f), "Checksum fail: " + f);
+//            }
+            assertTrue(VATINCheckDigit.getInstance().isValid(f), "Checksum fail: " + f);
+            assertTrue(VALIDATOR.hasValidator(f), "Missing validator: " + f);
+            assertTrue(VALIDATOR.isValid(f), f);
+        }
+    }
+}
