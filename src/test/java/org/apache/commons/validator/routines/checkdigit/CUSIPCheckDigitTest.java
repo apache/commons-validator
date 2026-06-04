5a3b88af05a5f78b4f644d8dc6917830014a5257
Gary Gregory <garydgregory@gmail.com>
2026-06-04 11:18:40 -0400

    Use @@MethodSource and add more CUSIP fixtures

--- src/test/java/org/apache/commons/validator/routines/checkdigit/CUSIPCheckDigitTest.java
+++ src/test/java/org/apache/commons/validator/routines/checkdigit/CUSIPCheckDigitTest.java
@@ -23,15 +23,20 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * CUSIP Check Digit Test.
  */
 class CUSIPCheckDigitTest extends AbstractCheckDigitTest {
 
-    private static final String[] VALID = new String[] { "DUS0421C5", "037833100", "931142103", "837649128", "392690QT3", "594918104", "86770G101", "Y8295N109",
+    private static final String[] INVALID = { "DUS0421CW", "DUS0421CN", "DUS0421CE", "0378#3100" };
+
+    private static final String[] VALID = { "DUS0421C5", "037833100", "931142103", "837649128", "392690QT3", "594918104", "86770G101", "Y8295N109",
             "G8572F100", "17275R102", "EJ7125481" };
+
+    static String[] cloneInvalid() {
+        return INVALID.clone();
+    }
 
     static String[] cloneValid() {
         return VALID.clone();
@@ -44,11 +49,11 @@ class CUSIPCheckDigitTest extends AbstractCheckDigitTest {
     protected void setUp() {
         routine = CUSIPCheckDigit.CUSIP_CHECK_DIGIT;
         valid = cloneValid();
-        invalid = new String[] { "0378#3100" };
+        invalid = cloneInvalid();
     }
 
     @ParameterizedTest
-    @ValueSource(strings = { "DUS0421CW", "DUS0421CN", "DUS0421CE" })
+    @MethodSource("org.apache.commons.validator.routines.checkdigit.CUSIPCheckDigitTest#cloneInvalid")
     void testValidator336InvalidCheckDigits(final String invalidCheckDigit) {
         assertFalse(routine.isValid(invalidCheckDigit), "Should fail: " + invalidCheckDigit);
     }
