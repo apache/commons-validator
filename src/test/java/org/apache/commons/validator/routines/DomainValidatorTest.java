b608cb524f29d7f3fc45a7bb13efeb77d72aec24
Sahana Bogar <sahvx655@gmail.com>
2026-07-23 14:47:12 +0530

    cover zero-width non-joiner in testIDNFormatCodePoints

--- src/test/java/org/apache/commons/validator/routines/DomainValidatorTest.java
+++ src/test/java/org/apache/commons/validator/routines/DomainValidatorTest.java
@@ -465,6 +465,7 @@ public class DomainValidatorTest {
         // Those code points are not legal in a host name and must be rejected.
         assertFalse(validator.isValid("exa\u00ADmple.com"), "soft hyphen shouldn't validate");
         assertFalse(validator.isValid("exa\u200Bmple.com"), "zero-width space shouldn't validate");
+        assertFalse(validator.isValid("exa\u200Cmple.com"), "zero-width non-joiner shouldn't validate");
         assertFalse(validator.isValid("exa\u200Dmple.com"), "zero-width joiner shouldn't validate");
         assertFalse(validator.isValid("\uFEFFexample.com"), "byte order mark shouldn't validate");
         assertTrue(validator.isValid("www.b\u00fccher.ch"), "b\u00fccher.ch should still validate");
